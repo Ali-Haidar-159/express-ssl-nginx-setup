e91fc4380c8b0720482c26d7d8a6d4cb54101be2
ALI  HAIDAR <alihaidar379227@gmail.com>
2025-05-20 18:32:30 +0600

    initial commit

---
+++ SSL server/basic/app.js
@@ -0,0 +1,67 @@
+// initial code 
+
+"use strict";
+console.clear();
+
+// Starts The Main Code : 
+
+// Require All The Modules , Packages And Objects : 
+
+let express = require("express") ;
+let app = express() ;
+
+let path = require("path") ;
+let fs = require("fs") ;
+let https = require("https") ;
+
+//Creating Server : 
+
+let myServer = https.createServer({
+
+    key : fs.readFileSync(path.join(__dirname , "ssl_config" , "key.pem")) ,
+    cert : fs.readFileSync(path.join(__dirname , "ssl_config" , "cert.pem"))
+
+} , app) ;
+
+// Connect With Server : 
+
+
+
+// Request-Response-Cycle : 
+
+app.get("/" , function(req,res){
+
+    res.status(200).send("<h1>This is HOME page</h1>") ;
+
+});
+
+// handle the route error 
+
+app.use(function(req,res,next){
+
+    res.status(404).json({
+
+        status : 404 ,
+        message : "Page not found !" 
+
+    });
+
+});
+
+// handle the server error 
+
+app.use(function(err,req,res,next){
+
+    res.status(500).json({
+
+        status : 500 ,
+        message : "Find the server error !!!" ,
+        error : err
+
+    });
+
+});
+
+// Exports Code :
+
+module.exports = myServer ;
